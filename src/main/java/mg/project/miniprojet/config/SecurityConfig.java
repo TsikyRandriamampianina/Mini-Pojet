497174f02cf20042e4cb4483654c69663134e84e
Tsiky Aro <arotsiky59@gmail.com>
2026-03-26 20:17:08 +0300

    Spring Security + JWT Filter ;

--- src/main/java/mg/project/miniprojet/config/SecurityConfig.java
+++ src/main/java/mg/project/miniprojet/config/SecurityConfig.java
@@ -3,22 +3,36 @@ package mg.project.miniprojet.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import mg.project.miniprojet.security.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtFilter;
+    public SecurityConfig(JwtAuthenticationFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       http
+           .csrf(csrf -> csrf.disable())
+           .sessionManagement(session ->
+               session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+           )
+           .authorizeHttpRequests(auth -> auth
+               .requestMatchers("/api/auth/**").permitAll()
+               .requestMatchers("/h2/**").permitAll()
+               .anyRequest().authenticated()
+           )
+           .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+           .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
-        http
-            .csrf(csrf -> csrf.disable())
-            .headers(headers -> headers.frameOptions(frame -> frame.disable()))
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/h2/**").permitAll()
-                .anyRequest().permitAll()
-            );
 
-        return http.build();
+       return http.build();
     }
 }
